20879e80fb473d1d4541fc0712d8462f36b189c5
ggaurav716 <gaurav.gupta716@gmail.com>
2019-02-07 20:27:59 +0530

    employee_second

--- abc/src/abc/ghiu.java
+++ abc/src/abc/ghiu.java
@@ -1,30 +1,21 @@
 package abc;
 
-class neew 
-{
+class neew {
 
-	
-	public static void main(String[] args) 
-	{
-	
+	public static void main(String[] args) {
+
 		System.out.println("neww");
 		System.out.println("repo2 change");
-	} 
-	
-	
+		System.out.println("repo2 change111111111111111111111111");
+	}
+
 }
 
+public class ghiu {
 
+	public static void main(String[] args) {
 
-public class ghiu 
-{
-
-	
-	public static void main(String[] args) 
-	{
-	
 		System.out.println("hello");
-	} 
-	
-	
+	}
+
 }
