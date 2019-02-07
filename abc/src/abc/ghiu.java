03ffdbbfef3742db832af4f38b71b0baee5aec15
ggaurav716 <gaurav.gupta716@gmail.com>
2019-02-07 15:55:33 +0530

    first commit!!!!!!!!!!!!!!!

--- abc/src/abc/ghiu.java
+++ abc/src/abc/ghiu.java
@@ -1,8 +1,24 @@
 package abc;
 
+class neew 
+{
+
+	
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("neww");
+	} 
+	
+	
+}
+
+
+
 public class ghiu 
 {
 
+	
 	public static void main(String[] args) 
 	{
 	
